8f5dae7987d18b2c655e49963609a522e49f3b69
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-06-30 01:15:31 -0600

    final fix

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,6 +19,8 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', '*')
+            ->header('Access-Control-Allow-Credentials', true)
+            ->header('Access-Control-Allow-Headers', '*')
             ->header('Accept', 'application/json');
     }
 }
