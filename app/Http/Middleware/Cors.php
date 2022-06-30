b03add2d0007595d6480fa0adcc8199cc1773fa4
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-06-30 01:13:40 -0600

    cutting some headers

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,8 +19,6 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Credentials', true)
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
             ->header('Accept', 'application/json');
     }
 }
