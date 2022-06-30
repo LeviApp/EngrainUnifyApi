8e5c1e480f114db38697d08d206756fb1d0e95fa
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-06-30 01:10:42 -0600

    fixer

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,7 +20,7 @@ class Cors
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', '*')
             ->header('Access-Control-Allow-Credentials', true)
-            ->header('Access-Control-Allow-Headers', '*')
-            ->header('Accept', '*');
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
+            ->header('Accept', 'application/json');
     }
 }
